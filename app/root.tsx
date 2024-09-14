f1df50868cff95c504d5b3a274d9753cedb4930e
Legend-Bgd <israelbgd1@gmail.com>
2024-09-14 02:39:04 +0100

    imported styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,13 @@
+import type { LinksFunction } from "@remix-run/node";
+// existing imports
+
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+
 import {
   Form,
   Links,
